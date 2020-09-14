448f3d56be7dbee1bcc0fbc7c371c9411a692741
Stephanie <user.name>
2020-09-14 16:10:17 -0700

    binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,6 +9,26 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    # find the midpoint element 
+    # length // 2
+    left = 0
+    right = len(arr) - 1
+    
+    # keep iterating so long as left <= right
+    while left <= right:
+        midpoint = (right + left) // 2
 
+        if arr[midpoint] == target:
+            return midpoint
+
+        elif arr[midpoint] > target:
+            # toss out the left side of the array 
+            # toss out the midpoint element itself as well 
+            right = midpoint - 1
+
+        else:
+            left = midpoint + 1
+
+    # we didn't find what we're looking for
 
     return -1  # not found
